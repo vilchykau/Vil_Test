1460688249b6d12a20a4d6b3a54dd8c58809c817
StasV <donsgenii@gmail.com>
2019-11-10 00:02:22 +0300

    Feat(TestComplete) In test state out 0 -> FAILED, 1 -> OK.

--- src/TestComplete.h
+++ src/TestComplete.h
@@ -23,13 +23,17 @@ namespace vtest{
       std::map<std::string, std::map<std::string, bool>> TestsStates;
   };
 
+  std::string BooleanToStatus(bool b){
+      return b ? "OK    " : "FAILED";
+  }
+
   std::ostream& operator<<(std::ostream& stream, TestComplete& testComplite){
       std::stringstream ss;
       ss << "\n#######################\n";
       ss << "State # Module name # Test name\n";
       for(const auto& module_pair : testComplite.GetTestsStates()){
           for(const auto& test_pair : module_pair.second){
-              ss << test_pair.second << "   " <<
+              ss << BooleanToStatus(test_pair.second) << "  " <<
                      module_pair.first << "  " <<
                      test_pair.first << '\n';
           }
