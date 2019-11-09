76fa6169c702e307e78651db67ab3bde61524fa2
StasV <donsgenii@gmail.com>
2019-11-09 22:35:13 +0300

    Created(VAL_TO_STR). Function that converts values to string.

--- src/utilities.h
+++ src/utilities.h
@@ -5,4 +5,15 @@
 #ifndef VIL_TEST_UTILITIES_H
 #define VIL_TEST_UTILITIES_H
 
+#include <string>
+#include <sstream>
+
+namespace vtest{
+    template <typename T1, typename T2>
+    std::string VAL_TO_STR(const T1 first, const T2 second){
+        std::stringstream ss;
+        ss << first << "  and  " << second;
+        return ss.str();
+    }
+}
 #endif //VIL_TEST_UTILITIES_H
