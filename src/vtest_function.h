f9202895fd49b9496644ee0911ae0ac462b55652
Stanislau Vilchykau <donsgenii@gmail.com>
2019-11-21 18:39:13 +0300

    Now exceptions show after test.

--- src/vtest_function.h
+++ src/vtest_function.h
@@ -35,45 +35,59 @@ namespace vtest{
 
     std::map<std::string, std::map<std::string, void (*)()>> TestModules;
 
-    bool RunFunction(const std::string& module_name, const std::string& test_name);
-
-    void AddTest(const std::string module_name, const std::string test_name, void (*fun)()){
-        TestModules[module_name][test_name] = fun;
+    bool RunFunction(const std::string& module_name, const std::string& test_name, std::vector<std::string>& mes);
+    void PrintMessages(std::vector<std::string> mes){
+        std::stringstream ss;
+        ss << "Failed asserts: " << '\n';
+        for(const auto& m : mes){
+            ss << m;
+        }
+        ss << '\n';
+        (*default_ostream) << ss.str();
     }
 
-    void RunTest(ALL_TESTS allTests){
+    void RunTest(const ALL_TESTS& allTests){
+        std::vector<std::string> messages_list;
         TestComplete testComplete;
         for(const auto& module_pair : TestModules){
             for(const auto& test_pair : module_pair.second){
-                testComplete.SetTestStatus(module_pair.first, test_pair.first, RunFunction(module_pair.first, test_pair.first));
+                testComplete.SetTestStatus(module_pair.first, test_pair.first, RunFunction(module_pair.first,
+                        test_pair.first, messages_list));
             }
         }
         (*default_ostream) << testComplete;
+        PrintMessages(messages_list);
     }
 
-    void RunTest(MODULE module){
+    void RunTest(const MODULE& module){
+        std::vector<std::string> messages_list;
         TestComplete testComplete;
         for(const auto& test_pair : TestModules[module.name]){
-            testComplete.SetTestStatus(module.name, test_pair.first, RunFunction(module.name, test_pair.first));
+            testComplete.SetTestStatus(module.name, test_pair.first, RunFunction(module.name,
+                    test_pair.first, messages_list));
         }
         (*default_ostream) << testComplete;
+        PrintMessages(messages_list);
     }
 
-    void RunTest(TEST test){
+    void RunTest(const TEST& test){
+        std::vector<std::string> messages_list;
         TestComplete testComplete;
-        testComplete.SetTestStatus(test.module, test.test, RunFunction(test.module, test.test));
+        testComplete.SetTestStatus(test.module, test.test, RunFunction(test.module,
+                test.test, messages_list));
         (*default_ostream) << testComplete;
+        PrintMessages(messages_list);
     }
 
-    bool RunFunction(const std::string& module_name, const std::string& test_name){
+    bool RunFunction(const std::string& module_name, const std::string& test_name, std::vector<std::string>& mes){
         try{
             TestModules[module_name][test_name]();
             return true;
         }catch (FailedAssertException& e){
             std::stringstream ss;
             ss << "Failed in Module: " << module_name <<
-                                    "   Test: " << test_name << '\n' << e.what() << '\n' << std::endl;
-            (*default_ostream) << ss.str();
+                                    "   Test: " << test_name << '\n' << e.what() << std::endl;
+            mes.push_back(ss.str());
             return false;
         }
     }
